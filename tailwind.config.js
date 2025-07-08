c79d31472aad05463e724cad76b13dc012b9fb91
maull7 <rmgaming601@gmail.com>
2025-07-08 14:13:09 +0700

    pembaruan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
- /** @type {import('tailwindcss').Config} */
- export default {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+
+  darkMode: 'class', // ⬅️ INI YANG DITAMBAHIN
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
